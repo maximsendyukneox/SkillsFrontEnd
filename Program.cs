05f84ffa02bcddc77b02b0a1dbca87fc0f99a9ae
Richard Peifer <code@report-r.de>
2023-08-07 11:01:44 +0200

    [m] Anpassung an Umbenennung newContext() -> createContext()

--- Program.cs
+++ Program.cs
@@ -4,46 +4,45 @@ using SkillsDatabase;
 using SkillsFrontEnd.Data;
 
 
-namespace SkillsFrontEnd
+namespace SkillsFrontEnd;
+
+public class Program
 {
-    public class Program
-    {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor.
-        public static SkillsDbContextFactory dbContextFactory { get; private set; } //TODO: Refactor static variable into WebApplication service
+    public static SkillsDbContextFactory dbContextFactory { get; private set; } //TODO: Refactor static variable into WebApplication service
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor.
 
-        public static void Main(string[] args)
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        dbContextFactory = new SkillsDbContextFactory();
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        //builder.Services.AddSingleton<WeatherForecastService>();
+        
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            dbContextFactory = new SkillsDbContextFactory();
-
-            // Add services to the container.
-            builder.Services.AddRazorPages();
-            builder.Services.AddServerSideBlazor();
-            //builder.Services.AddSingleton<WeatherForecastService>();
-            
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.MapBlazorHub();
-            app.MapFallbackToPage("/_Host");
-
-            app.Run();
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/_Host");
+
+        app.Run();
     }
 }
