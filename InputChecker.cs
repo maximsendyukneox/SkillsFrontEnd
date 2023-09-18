5b98bf73901825b386dd9ec22a6b6b7a46475171
maximsendyukneox <maxim.sendyuk@neox.de>
2023-09-18 19:20:41 +0200

    Created Proficiency create-, update and get- API-methods, the corresponding client methods, migrated employee creation-, search-, and updating- functionality completely to work through API, added more sanity checks on input

--- InputChecker.cs
+++ InputChecker.cs
@@ -19,11 +19,33 @@
             }
         }
 
+        public static void ValidateDateOfBirth( DateTime dob ) 
+        {
+            if (dob > DateTime.Now)
+            {
+                throw new ArgumentException("The date of birth must not be in the future");
+            }
+        }
+
         public static void ValidateSkill( string skillName )
         {
             if (skillName == String.Empty)
             {
                 throw new ArgumentException("The name must not be empty");
+            }
+        }
+
+        public static void FindSkillDuplicates(List<string> skills)
+        {
+            foreach(string skill1 in skills)
+            {
+                foreach (string skill2 in skills)
+                {
+                    if(skill1 == skill2 && !object.ReferenceEquals(skill1, skill2))
+                    {
+                        throw new ArgumentException("One employee may not have the same skill twice");
+                    }
+                }
             }
         }
     }
