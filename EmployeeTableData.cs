807f10824e0ba46aaaa6f7e10466927c4d96305f
Lukas Zoellner <lukasmz91@gmail.com>
2023-10-08 16:17:21 +0200

    improved displaying search Results and minor style changes

---
+++ EmployeeTableData.cs
@@ -0,0 +1,13 @@
+﻿using SkillsDatabase;
+
+namespace SkillsFrontEnd
+{
+    public class EmployeeTableData
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public DateTime Birthday { get; set; } = DateTime.MinValue;
+        public List<string> Skills { get; set; } = new List<string>();
+        public List<ProficiencyLevel> SkillLevels { get; set; } = new List<ProficiencyLevel>();
+    }
+}
