1cb146b321fcc917ac9723fb593f3b3b0657b832
Lukas Zoellner <lukasmz91@gmail.com>
2023-10-09 00:10:37 +0200

    improved validation and small fix for skill based search
    ...fix: clear tableData for another search, so the entries do not stack

--- SaveEmployeeForm.cs
+++ SaveEmployeeForm.cs
@@ -6,12 +6,12 @@ namespace SkillsFrontEnd
     {
         [Required(ErrorMessage = "Bitte einen Vornamen eingeben")]
         [StringLength(20, ErrorMessage = "Der Name darf nicht länger als 20 Zeichen sein")]
-        [RegularExpression(@"^[A-Za-zöÖüÜäÄß-]*$", ErrorMessage ="Keine Sonderzeichen oder Zahlen erlaubt")]
+        [RegularExpression(@"^[A-Za-zöÖüÜäÄß][A-Za-zöÖüÜäÄß\s-]*$", ErrorMessage ="Keine Sonderzeichen oder Zahlen erlaubt")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Bitte einen Nachnamen eingeben")]
         [StringLength(20, ErrorMessage = "Der Name darf nicht länger als 20 Zeichen sein")]
-        [RegularExpression(@"^[A-Za-zöÖüÜäÄß-]*$", ErrorMessage = "Keine Sonderzeichen oder Zahlen erlaubt")]
+        [RegularExpression(@"^[A-Za-zöÖüÜäÄß][A-Za-zöÖüÜäÄß\s-]*$", ErrorMessage = "Keine Sonderzeichen oder Zahlen erlaubt")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Bitte ein gültiges Geburtsdatum eingeben")]
