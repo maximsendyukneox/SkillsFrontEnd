6aa6c8e030b2f62638faa602761793d101f3ea97
Lukas Zoellner <lukasmz91@gmail.com>
2023-11-15 14:14:45 +0100

    show message when initial search doesnt find results, added default sorting for table, allow ß in inputs

--- SaveEmployeeForm.cs
+++ SaveEmployeeForm.cs
@@ -6,12 +6,12 @@ namespace SkillsFrontEnd
     {
         [Required(ErrorMessage = "Bitte einen Vornamen eingeben")]
         [StringLength(20, ErrorMessage = "Der Name darf nicht länger als 20 Zeichen sein")]
-        [RegularExpression(@"^[a-zA-ZöäüÖÄÜ]+([ -][a-zA-ZöäüÖÄÜ]+)?$", ErrorMessage ="Keine Sonderzeichen oder Zahlen erlaubt")]
+        [RegularExpression(@"^[a-zA-ZöäüÖÄÜß]+([ -][a-zA-ZöäüÖÄÜß]+)?$", ErrorMessage ="Keine Sonderzeichen oder Zahlen erlaubt")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Bitte einen Nachnamen eingeben")]
         [StringLength(20, ErrorMessage = "Der Name darf nicht länger als 20 Zeichen sein")]
-        [RegularExpression(@"^[a-zA-ZöäüÖÄÜ]+([ -][a-zA-ZöäüÖÄÜ]+)?$", ErrorMessage = "Keine Sonderzeichen oder Zahlen erlaubt")]
+        [RegularExpression(@"^[a-zA-ZöäüÖÄÜß]+([ -][a-zA-ZöäüÖÄÜß]+)?$", ErrorMessage = "Keine Sonderzeichen oder Zahlen erlaubt")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Kein valides Datumsformat")]
